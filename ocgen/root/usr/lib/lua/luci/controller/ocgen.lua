bfdcc71d31fb2efea91b50b2e9f5f989bcb0cc89
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2024-10-17 00:09:04 +0700

    Update ocgen.lua

--- ocgen/root/usr/lib/lua/luci/controller/ocgen.lua
+++ ocgen/root/usr/lib/lua/luci/controller/ocgen.lua
@@ -1,5 +1,5 @@
 module("luci.controller.ocgen", package.seeall)
 function index()
 	entry({"admin", "services"}, firstchild(), "services", 44).dependent=false
-	entry({"admin", "sercives", "ocgen"}, template("ocgen"), _("ocgen"), 55).dependent=true
+	entry({"admin", "services", "ocgen"}, template("ocgen"), _("ocgen"), 55).dependent=true
 end
